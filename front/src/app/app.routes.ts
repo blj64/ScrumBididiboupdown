2c3ac459f4e80ca4dc408b99309ccd5615ae7ee8
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-02-04 16:32:43 +0100

    landing page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { TeamCreationComponent } from './components/team-creation/team-creation.component';
 import { AuthGuard } from './guard/auth.guard';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'teamcreation', component: TeamCreationComponent, canActivate: [AuthGuard]},
+    {path: 'home', component: HomeComponent},
+    {path: '',redirectTo: 'home', pathMatch: 'full'}
 
 ];
