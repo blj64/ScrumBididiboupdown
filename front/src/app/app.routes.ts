93eb5f24d46b7c974db64560fdd2880de81b9274
blj64 <jliogerbun@gmail.com>
2025-02-04 11:07:47 +0100

    routes fix

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-
+import { TeamCreationComponent } from './components/team-creation/team-creation.component';
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
+    {path: 'teamcreation', component: TeamCreationComponent},
+
 ];
